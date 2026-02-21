ac7da847567285a114d5f77278371eb22db57b0f
Alexainc <rasanjanahansaka2006@gmail.com>
2026-02-21 22:16:00 +0530

    bring ai function back 2

--- bot.js
+++ bot.js
@@ -5694,7 +5694,7 @@ from : @${visibleNumber}
                     const groupId = msg.key.remoteJid;
 
                     const isReplyToBot = msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.participant === botJid || msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.participant === botLid;
-                    console.log(msg.message ?);
+                    console.log(msg.message);
                     if (!isGroup) {
                         // ✅ Not a group → run AI
                         runAI();
